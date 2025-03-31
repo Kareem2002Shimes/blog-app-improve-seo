9647161d81a933bddbb48c3699feb3c02a763b45
kareem2002shimes <kareem4shimas@gmail.com>
2025-03-31 14:18:38 +0200

    add metadata for layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,10 +1,16 @@
 import { Roboto } from "next/font/google";
 import "./globals.css";
+import { Metadata } from "next";
 
 const robotoSans = Roboto({
   variable: "--font-roboto-sans",
   subsets: ["latin"],
 });
+
+export const metadata: Metadata = {
+  title: "My Next.js App",
+  description: "A simple Next.js application",
+};
 
 export default function RootLayout({
   children,
